a4b534371d8850be1538fe8ea2d6b4fe0e7137c1
s-rayapudi <sam@example.comsandeep.rayapudi.in@gmail.com>
2026-05-30 13:00:44 +0530

    Added Utilties for reading env information from JSON and Properties file

---
+++ src/test/java/com/utility/JSONUtility.java
@@ -0,0 +1,33 @@
+package com.utility;
+
+import java.io.File;
+import java.io.FileReader;
+
+import com.constants.Environments;
+import com.google.gson.Gson;
+import com.ui.pojos.Config;
+import com.ui.pojos.Environment;
+
+public class JSONUtility {
+
+    public static String readJSON(Environments env) throws Exception {
+        Gson gson = new Gson();
+        File jsonFile = new File(System.getProperty("user.dir") + File.separator + "config" + File.separator + "config.json");
+
+        try (FileReader fileReader = new FileReader(jsonFile)) {
+            Config config = gson.fromJson(fileReader, Config.class);
+            if (config == null || config.getEnvironments() == null) {
+                throw new Exception("Invalid JSON config structure: missing environments object");
+            }
+
+            Environment environment = config.getEnvironments().get(env.name());
+            if (environment == null || environment.getUrl() == null || environment.getUrl().isEmpty()) {
+                throw new Exception("URL not defined for environment: " + env.name());
+            }
+
+            return environment.getUrl();
+        } catch (Exception e) {
+            throw new Exception("Failed to read JSON config file: " + jsonFile.getAbsolutePath(), e);
+        }
+    }
+}
